18ea0168c27c7be63e33d41a22e69889b14683b7
chrisamcelveen <camcelve@gmail.com>
2016-05-15 23:33:38 -0400

    pushing to master.

---
+++ client/app/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('meanApp', [
+  'meanApp.auth',
+  'meanApp.admin',
+  'meanApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'btford.socket-io',
+  'ui.router',
+  'ui.bootstrap',
+  'validation.match'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
